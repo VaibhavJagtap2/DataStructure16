2df0925b6ab99c0dfb26bdc57143db623db72153
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-10 16:40:21 +0530

    Add:Linked List Appending

--- src/main/java/com/javalinkedlist/MyLinkedList.java
+++ src/main/java/com/javalinkedlist/MyLinkedList.java
@@ -4,31 +4,30 @@ public class MyLinkedList<K> {
     public INode<K> head;
     public INode<K> tail;
     public MyLinkedList() {
-        this.head=head;
-        this.tail=tail;
+        this.head = head;
+        this.tail = tail;
     }
     public void add(INode<K> newNode) {
-        if(this.tail==null) {
-            this.tail=newNode;
+        if(this.tail == null) {
+            this.tail = newNode;
         }
-        if(this.head==null) {
-            this.head=newNode;
+        if(this.head == null) {
+            this.head = newNode;
         }
         else {
-            INode<K> tempNode=this.head;
-            //INode newNode;
-            this.head=newNode;
+            INode<K> tempNode = this.head;
+            this.head = newNode;
             this.head.setNext(tempNode);
         }
     }
     public void printMyNodes() {
-        StringBuffer myNodes=new StringBuffer("My Nodes: ");
-        INode tempNode=head;
-        while(tempNode.getNext()!=null) {
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while(tempNode.getNext() != null) {
             myNodes.append(tempNode.getKey());
             if(!tempNode.equals(tail))
                 myNodes.append("->");
-            tempNode=tempNode.getNext();
+            tempNode = tempNode.getNext();
         }
         myNodes.append(tempNode.getKey());
         System.out.println(myNodes);
--- src/main/java/com/javalinkedlist/MyNode.java
+++ src/main/java/com/javalinkedlist/MyNode.java
@@ -1,8 +1,8 @@
 package com.javalinkedlist;
 
 public class MyNode<K> implements INode<K> {
-    private K key;
-    private INode next;
+    public K key;
+    public INode next;
 
     public MyNode(K key) {
         this.key = key;
@@ -18,7 +18,6 @@ public class MyNode<K> implements INode<K> {
     public void setKey(K key) {
         this.key = key;
     }
-
     public INode<K> getNext() {
         return next;
     }
@@ -26,4 +25,13 @@ public class MyNode<K> implements INode<K> {
     public void setNext(INode next) {
         this.next = (MyNode<K>) next;
     }
+    @Override
+    public String toString() {
+        StringBuilder MyNodeString = new StringBuilder();
+        MyNodeString.append("MyNode{" + "Key=").append(key).append('}');
+        if (next != null)
+            MyNodeString.append("->").append(next);
+        return MyNodeString.toString();
+    }
 }
+
