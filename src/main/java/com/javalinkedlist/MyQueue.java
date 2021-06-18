f60fac237c0dd1ceb38dc930480af3599da94b68
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-18 22:02:28 +0530

    Add:dequeue the element

--- src/main/java/com/javalinkedlist/MyQueue.java
+++ src/main/java/com/javalinkedlist/MyQueue.java
@@ -1,20 +1,19 @@
 package com.javalinkedlist;
 public class MyQueue {
     private final MyLinkedList myLinkedList;
-
     public MyQueue() {
         this.myLinkedList = new MyLinkedList();
     }
-
     public void queue(INode myNode) {
         myLinkedList.append(myNode);
     }
-
     public void printQueue() {
         myLinkedList.printMyNodes();
     }
-
     public INode peak() {
         return myLinkedList.head;
+    }
+    public INode dequeue() {
+        return myLinkedList.pop();
     }
 }
--- src/test/java/com/javalinkedlist/QueueTest.java
+++ src/test/java/com/javalinkedlist/QueueTest.java
@@ -17,4 +17,16 @@ public class QueueTest {
         INode peak = myQueue.peak();
         Assert.assertEquals(peak,myFirstNode);
     }
+    @Test
+    public void given3NumbersWhenDeletePeakOfQueueShouldReturnTrue() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.queue(myFirstNode);
+        myQueue.queue(mySecondNode);
+        myQueue.queue(myThirdNode);
+        INode peak = myQueue.dequeue();
+        myQueue.printQueue();
+        Assert.assertEquals(peak, myFirstNode);
+    }
 }
