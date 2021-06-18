5cbe2a645e974f28214f732c3974091fcf59da00
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-18 20:09:24 +0530

    Add:Create Queue

---
+++ src/main/java/com/javalinkedlist/MyQueue.java
@@ -0,0 +1,20 @@
+package com.javalinkedlist;
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    public void queue(INode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+    public void printQueue() {
+        myLinkedList.printMyNodes();
+    }
+
+    public INode peak() {
+        return myLinkedList.head;
+    }
+}
---
+++ src/test/java/com/javalinkedlist/QueueTest.java
@@ -0,0 +1,20 @@
+package com.javalinkedlist;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QueueTest {
+    MyQueue myQueue = new MyQueue();
+    @Test
+    public void given3NumbersWhenAddedTOQueueShouldReturnTrue() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.queue(myFirstNode);
+        myQueue.queue(mySecondNode);
+        myQueue.queue(myThirdNode);
+        myQueue.printQueue();
+        INode peak = myQueue.peak();
+        Assert.assertEquals(peak,myFirstNode);
+    }
+}
